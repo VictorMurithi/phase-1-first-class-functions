81dd6a430fbca0d55a10f96fdc110ee30e94920a
victor <victor.murithi@student.moringaschool.com>
2023-09-20 10:39:03 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+
+function receivesAFunction(callbackFunction){
+    callbackFunction();
+}
+function returnsANamedFunction(){
+    const namedFunction = hook => {
+        console.log("before all");
+    };
+    return namedFunction;
+}
+const myFunction = returnsANamedFuncion();
+myFunction();
+
+function returnsAnAnonymousFunction(){
+    return function(){
+       console.log("This is an anonymous function");
+    };
+}
